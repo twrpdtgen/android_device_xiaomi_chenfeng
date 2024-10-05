997cea575ff0e26b6132b6bd6c474ad564d05f5b
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-10-05 21:33:16 +0200

    chenfeng: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: 24053PY09I Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Xiaomi 24053PY09I (chenfeng)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_chenfeng.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from chenfeng device
+$(call inherit-product, device/xiaomi/chenfeng/device.mk)
+
+PRODUCT_DEVICE := chenfeng
+PRODUCT_NAME := omni_chenfeng
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := 24053PY09I
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="chenfeng_global-user 14 UKQ1.240116.001 V816.0.5.0.UNJINXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/chenfeng_global/chenfeng:14/UKQ1.240116.001/V816.0.5.0.UNJINXM:user/release-keys
